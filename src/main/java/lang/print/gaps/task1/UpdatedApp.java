f3da541d121b8f6574115e691208ee99d274405c
Muhammed Abdrahman <muhammed03amin.com>
2022-08-15 23:34:01 +0600

    fix: update logic

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -2,7 +2,6 @@ package lang.print.gaps.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.println("I am updated");
-        System.out.println("can be compiled");
+        System.out.println("I'm updated" + "\n" + "can be compiled");
     }
 }
