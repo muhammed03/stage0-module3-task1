d2c57c9c91733bf36bce37967619d5ff6d8deb77
Muhammed Abdrahman <muhammed03amin.com>
2022-08-16 00:26:33 +0600

    fix: update logic

--- src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
+++ src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
@@ -1,7 +1,7 @@
 package lang.print.gaps.task1;
 
 public class PrintSentenceApp {
-    public static void main(String[] args) {
-        System.out.println("java" + "\n" + "is" + "\n" +"smth");
-    }
+  public static void main(String[] args) {
+    System.out.println("This\nis\nmy\nfirst\nJava\nprogram");
+  }
 }
