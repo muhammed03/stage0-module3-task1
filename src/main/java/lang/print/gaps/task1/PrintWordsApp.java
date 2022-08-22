88d1bb2e67a96e56f969f1805cee24a90a17d1ef
Muhammed Abdrahman <muhammed03amin.com>
2022-08-22 23:55:17 +0600

    fix: update tasks

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -1,7 +1,7 @@
 package lang.print.gaps.task1;
 
 public class PrintWordsApp {
-    public static void main(String[] args) {
-        System.out.println("This\nis\nmy\nfirst\nJava\nprogram");
-    }
+  public static void main(String[] args) {
+    System.out.println("java" + "\n" + "is" + "\n" + "smth");
+  }
 }
